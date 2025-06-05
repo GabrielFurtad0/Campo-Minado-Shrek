00785cfb072174c1fa61e1ae4d17847b434ce739
Guilherme-Silva-Oliveira <guilherme.soliveira@sptech.school>
2025-06-05 12:14:57 -0300

    final

--- PROJETO - VETOR/script.js
+++
@@ -1,6 +0,0 @@
-function comecar(){
-    let inicio = document.getElementById('inicio');
-    inicio.style.display = 'none';
-    let jogo = document.getElementById('jogo');
-    jogo.style.display = 'block'
-}
