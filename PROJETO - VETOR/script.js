bea2b6cf7f4f35b5fbcf9b3e5056c0396d209082
Guilherme-Silva-Oliveira <guilherme.soliveira@sptech.school>
2025-06-05 10:53:52 -0300

    EnviandoEstrutura

---
+++ PROJETO - VETOR/script.js
@@ -0,0 +1,6 @@
+function comecar(){
+    let inicio = document.getElementById('inicio');
+    inicio.style.display = 'none';
+    let jogo = document.getElementById('jogo');
+    jogo.style.display = 'block'
+}
